1ed2357b94d51c75913a5b9256c049a06828e5f4
Hongliang Ma <mhl1983@gmail.com>
2017-08-16 17:35:36 +0800

    bugfix

--- train_real_data.py
+++ train_real_data.py
@@ -7,11 +7,14 @@ import numpy as np
 
 h5file = h5py.File('captchas/dataset.h5', 'r')
 X = np.asarray(h5file['X'])
-y = np.asarray(h5file['y'])
+yy = np.asarray(h5file['y'])
+y = []
+for i in range(yy.shape[0]):
+    y.append(yy[i])
 
 model = network.create_model()
 
-model.fit(X, y, batch_size=32, epochs=1, validation_split=0.1)
+model.fit(X, y, batch_size=32, epochs=5, validation_split=0.1)
 
 model.save_weights('my_model_weights_real_data.h5')
 
