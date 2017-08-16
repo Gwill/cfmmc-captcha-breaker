d002d246fb76ff5dddcd2b9edd8bd6d25c9848be
Hongliang Ma <mhl1983@gmail.com>
2017-08-16 17:51:24 +0800

    bugfix: predict.py

--- predict.py
+++ predict.py
@@ -1,10 +1,12 @@
 # coding: utf8
 
 import PIL
+import numpy as np
+import network
 from configs import *
 
 
-def predict(img):
+def predict(model, img):
     X = np.zeros((1, HEIGHT, WIDTH, NUM_CHANNELS), dtype=np.uint8)
     X[0] = img
     y = model.predict(X)
